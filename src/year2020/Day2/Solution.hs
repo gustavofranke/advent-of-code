f4c0da9493584e958c6c56aadaa15d10a28d1f05
Gustavo Franke <franke.gustavo@gmail.com>
2020-12-04 09:07:40 +0000

    reuse more to be able to test more in day 2

--- src/year2020/Day2/Solution.hs
+++ src/year2020/Day2/Solution.hs
@@ -1,39 +1,69 @@
 module Day2.Solution where
 
-import Inputs
+import Inputs ( day2 )
+
+data PolicyWithPasswd = PolicyWithPasswd
+                      { mini :: Int
+                      , maxi :: Int
+                      , char :: Char
+                      , pass :: String
+                      }
+
+mkPolicyPass :: String -> PolicyWithPasswd
+mkPolicyPass str = PolicyWithPasswd (read minimu :: Int) (read maximu :: Int) (head charac) passwd
+  where
+    policy = takeWhile (/= ':') str
+    minMax = takeWhile (/= ' ') policy
+    minimu = takeWhile (/= '-') minMax
+    maximu = tail $ dropWhile (/= '-') minMax
+    charac = tail $ dropWhile (/= ' ') policy
+    passwd = drop 2 $ dropWhile (/= ':') str
 
 -- |
--- >>> isValidA 2 5 'l' "fllxf"
+-- >>> isValidA (PolicyWithPasswd 2 5 'l' "fllxf")
 -- True
 -- 
--- >>> isValidA 4 5 'r' "rrrjmrrrrrrh"
+-- >>> isValidA (PolicyWithPasswd 4 5 'r' "rrrjmrrrrrrh")
 -- False
-isValidA :: Int -> Int -> Char -> String -> Bool
-isValidA mini maxi char pass = nbrChar >= mini && nbrChar <= maxi
+isValidA :: PolicyWithPasswd -> Bool
+isValidA pwp = nbrChar >= mini pwp && nbrChar <= maxi pwp
    where
-      nbrChar = length $ filter (== char) pass
+      nbrChar = length $ filter (== char pwp) (pass pwp)
 
--- isValidA is for getting day2a answer --> 660
--- isValidB is for getting day2b answer --> 530
-parsePolicyPass :: String -> Bool
-parsePolicyPass str = isValidB (read minimu :: Int) (read maximu :: Int) (head charac) passwd
-   where
-      policy = takeWhile (/= ':') str
-      minMax = takeWhile (/= ' ') policy
-      minimu = takeWhile (/= '-') minMax
-      maximu = tail $ dropWhile (/= '-') minMax
-      charac = tail $ dropWhile (/= ' ') policy
-      passwd = drop 2 $ dropWhile (/= ':') str
+parse :: (PolicyWithPasswd -> Bool) -> String -> Bool
+parse isValid str = isValid $ mkPolicyPass str
 
-day2a = length $ filter (==True) (map parsePolicyPass Inputs.day2)
+answer :: (PolicyWithPasswd -> Bool) -> Int
+answer isValid = length $ filter (==True) (map (parse isValid) Inputs.day2)
 
+-- |
+-- >>> day2a
+-- 660
+day2a :: Int
+day2a = answer isValidA
+
+-- |
+-- >>> day2b
+-- 530
+day2b :: Int
+day2b = answer isValidB
+
+-- |
 -- 1-3 a: abcde is valid: position 1 contains a and position 3 does not.
 -- 1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
 -- 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
-isValidB :: Int -> Int -> Char -> String -> Bool
-isValidB min max char pass = isInMax || isInMin
+-- 
+-- >>> isValidB (PolicyWithPasswd 1 3 'a' "abcde")
+-- True
+-- >>> isValidB (PolicyWithPasswd 1 3 'b' "cdefg")
+-- False
+-- 
+-- >>> isValidB (PolicyWithPasswd 2 9 'c' "ccccccccc")
+-- False
+isValidB :: PolicyWithPasswd -> Bool
+isValidB pwp = isInMax || isInMin
    where
-      maxPos = pass !! (max - 1)
-      minPos = pass !! (min - 1)
-      isInMax = maxPos == char && minPos /= char
-      isInMin = maxPos /= char && minPos == char
+      maxPos = pass pwp !! (maxi pwp - 1)
+      minPos = pass pwp !! (mini pwp - 1)
+      isInMax = maxPos == char pwp && minPos /= char pwp
+      isInMin = maxPos /= char pwp && minPos == char pwp
