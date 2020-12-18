5dc95aee20e0f86028c1b523559029bd4361c0ca
Gustavo Franke <franke.gustavo@gmail.com>
2020-12-18 22:03:25 +0000

    add part 2, day 1, 2017

--- src/Year2017/Day1/Solution.hs
+++ src/Year2017/Day1/Solution.hs
@@ -1,6 +1,6 @@
 module Year2017.Day1.Solution where
 
-import Data.Char
+import Data.Char ( digitToInt )
 
 -- |
 -- >>> answerPart1 [1, 1, 2, 2]
@@ -29,3 +29,34 @@ part1 = do
   inp <- lines <$> readFile "src/Year2017/Day1/input.txt"
   return $ answerPart1 $ digitToInt <$> head inp
 
+-- |
+-- >>> answerPart2 [1,2,1,2]
+-- 6
+-- >>> answerPart2 [1,2,2,1]
+-- 0
+-- >>> answerPart2 [1,2,3,4,2,5]
+-- 4
+-- >>> answerPart2 [1,2,3,1,2,3]
+-- 12
+-- >>> answerPart2 [1,2,1,3,1,4,1,5]
+-- 4
+answerPart2 :: [Int] -> Int
+answerPart2 qs = go lengthForGo listForGo
+  where
+    lengthForGo = length qs `div` 2
+    listForGo = qs ++ take (lengthForGo + 1) qs
+    go _ [] = 0
+    go _ [_] = 0
+    go i (l1 : ls) =
+      let l2 = if i < length (l1 : ls) -1 then (l1 : ls) !! i else -999
+       in if l1 == l2
+            then l1 + go i ls
+            else go i ls
+
+-- |
+-- >>> part2
+-- 1292
+part2 :: IO Int
+part2 = do
+  inp <- lines <$> readFile "src/Year2017/Day1/input.txt"
+  return $ answerPart2 $ digitToInt <$> head inp
