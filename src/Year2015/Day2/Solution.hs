d258f4723837854a123d4156f7c0ba1e29001b82
Gustavo Franke <franke.gustavo@gmail.com>
2020-12-20 21:22:29 +0000

    add part2 day2 2015

--- src/Year2015/Day2/Solution.hs
+++ src/Year2015/Day2/Solution.hs
@@ -28,3 +28,32 @@ answerInput :: IO Int
 answerInput = do
   inp <- T.lines <$> DTIO.readFile "src/year2015/Day2/input.txt"
   return $ sum (fmap (\xs -> area (xs !! 0) (xs !! 1) (xs !! 2)) (parse <$> inp))
+
+------------
+-- The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter of any one face.
+-- Each present also requires a bow made out of ribbon as well;
+-- the feet of ribbon required for the perfect bow is equal to the cubic feet of volume of the present.
+
+-- For example:
+
+-- 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
+-- 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
+
+-- |
+-- >>> ribbon 2 3 4
+-- 34
+-- >>> ribbon 1 1 10
+-- 14
+ribbon :: Int -> Int -> Int -> Int
+ribbon l w h = shortest + bow
+  where
+    shortest = sum ((take 2 $ sort [l, w, h]) ++ (take 2 $ sort [l, w, h]))
+    bow = l * w * h
+
+-- |
+-- >>> answerInput2
+-- 3737498
+answerInput2 :: IO Int
+answerInput2 = do
+  inp <- T.lines <$> DTIO.readFile "src/year2015/Day2/input.txt"
+  return $ sum (fmap (\xs -> ribbon (xs !! 0) (xs !! 1) (xs !! 2)) (parse <$> inp))
