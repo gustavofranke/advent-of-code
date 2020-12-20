01664059518a2b98ca569f54fa7c63f49174ae0a
Gustavo Franke <franke.gustavo@gmail.com>
2020-12-20 11:42:52 +0000

    add part2 day1 2015

--- src/Year2015/Day1/Solution.hs
+++ src/Year2015/Day1/Solution.hs
@@ -36,3 +36,25 @@ part1 = do
   inp <- lines <$> readFile "src/year2015/Day1/input.txt"
   return $ findFloor $ head inp
 
+------------------
+-- |
+-- >>> findBasementPos ")"
+-- 1
+
+-- >>> findBasementPos "()())"
+-- 5
+findBasementPos :: [Char] -> Int
+findBasementPos = go (0 :: Int) (0 :: Int)
+  where
+    go res pos []         = if res == -1 then pos else 0
+    go res pos ('(' : ps) = if res == -1 then pos else go (res + 1) (pos + 1) ps
+    go res pos (')' : ps) = if res == -1 then pos else go (res - 1) (pos + 1) ps
+    go _ _ (p : _) = error ("invalid char" ++ show p)
+
+-- |
+-- >>> part2
+-- 1783
+part2 :: IO Int
+part2 = do
+  inp <- lines <$> readFile "src/year2015/Day1/input.txt"
+  return $ findBasementPos $ head inp
