285b0651fb8b5f52274e099fb44034ded703615b
Gustavo Franke <franke.gustavo@gmail.com>
2020-12-24 21:23:33 +0000

    add 2015 day 10

---
+++ src/Year2015/Day10/Solution.hs
@@ -0,0 +1,38 @@
+module Year2015.Day10.Solution where
+
+import Data.List ( group, nub )
+
+-- 1 becomes 11 (1 copy of digit 1).
+-- 11 becomes 21 (2 copies of digit 1).
+-- 21 becomes 1211 (one 2 followed by one 1).
+-- 1211 becomes 111221 (one 1, one 2, and two 1s).
+-- 111221 becomes 312211 (three 1s, two 2s, and one 1).
+
+-- |
+-- >>> asdf "1"
+-- "11"
+-- >>> asdf "11"
+-- "21"
+-- >>> asdf "21"
+-- "1211"
+-- >>> asdf "1211"
+-- "111221"
+-- >>> asdf "111221"
+-- "312211"
+asdf nbr = concatMap (\ns -> show (length ns) ++ nub ns) $ group nbr
+
+-- |
+-- >>> length $ answer 40
+-- 360154
+-- >>> length $ answer 50
+-- 5103798
+answer rep = foldl (\a f -> f a) "1113122113" (replicate rep asdf) --"1113122113"
+
+-- |
+-- >>> length $ go "1113122113" 40
+-- 360154
+-- >>> length $ go "1113122113" 50
+-- 5103798
+go str ctr
+    | ctr == 0 = str 
+    | otherwise = go (asdf str) (ctr -1)
